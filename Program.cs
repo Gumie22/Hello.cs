53bd97d094d13c86213768ba9fb0a81af93d1f76
Gumani Ratshisuka <62070830+Gumie22@users.noreply.github.com>
2020-10-30 15:34:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//This is a C# program that greets the user and displays their age
+using System;
+
+namespace program1.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SayHi("Gumie", 27);
+            SayHi("Irene", 64);
+            SayHi("Brian", 32);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello "+ name + " you are " + age + " years old");
+        }
+    }
+}
